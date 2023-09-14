6bbb0b6beb3343246bee2e53e2dbd32c44ad9e48
Will Gladstone <will.gl@dst.one>
2023-09-14 09:45:33 -0400

    placeholder

--- initdb/sql/triggers/trigger_20_create_fields_partitions.sql
+++ initdb/sql/triggers/trigger_20_create_fields_partitions.sql
@@ -2,16 +2,20 @@
  * trigger_20_create_fields_partitions: triggered after insert into fields table. Create a new data partition for the new id.
  * @depends trigger_create_fields_partitions FUNCTION (postgresql)
  **/
+/*
 DROP TRIGGER IF EXISTS trigger_20_create_fields_partitions ON fieldsets.fields;
 CREATE TRIGGER trigger_20_create_fields_partitions
 AFTER INSERT ON fieldsets.fields
 FOR EACH ROW
 EXECUTE FUNCTION fieldsets.trigger_create_fields_partitions();
+*/
 
-
+/*
 COMMENT ON TRIGGER trigger_20_create_fields_partitions ON fieldsets.fields IS '
+*/
 /**
  * trigger_20_create_fields_partitions: triggered after insert into fields table. Create a new data partition for the new id.
  * @depends trigger_create_fields_partitions FUNCTION (postgresql)
  * @priority 20
- */';
+ */
+ --';
