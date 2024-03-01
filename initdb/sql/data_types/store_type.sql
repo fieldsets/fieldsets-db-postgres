7ddb9693c93242d09d38c35c8f4c8fca623b951d
willgladstone <will.gl@dst.one>
2024-03-01 15:21:06 -0500

    schema integration scripts

--- initdb/sql/data_types/store_type.sql
+++ initdb/sql/data_types/store_type.sql
@@ -4,13 +4,12 @@ BEGIN
 	SELECT EXISTS(SELECT 1 FROM pg_catalog.pg_type WHERE typname='STORE_TYPE') INTO type_exists;
     IF NOT type_exists THEN
         EXECUTE format(
-            'CREATE TYPE STORE_TYPE AS ENUM (%L, %L, %L, %L, %L, %L, %L, %L, %L, %L, %L, %L, %L);',
+            'CREATE TYPE STORE_TYPE AS ENUM (%L, %L, %L, %L, %L, %L, %L, %L, %L, %L, %L, %L);',
             'filter',
             'lookup',
             'record',
             'document',
             'message',
-            'metadata',
             'sequence',
             'enum',
             'view',
--- initdb/sql/tables/pipeline.imports.sql
+++ initdb/sql/tables/pipeline.imports.sql
@@ -1,11 +1,11 @@
 /**
  * This imports table is a simple table that contains only an indexed JSONB column so we can easily import and query any json schemas.
  */
-CREATE TABLE IF NOT EXISTS fieldsets.imports(
+CREATE TABLE IF NOT EXISTS pipeline.imports(
     token TEXT NULL,
     source TEXT NULL,
     type TEXT NULL,
-    order INT NULL,
+    priority INT NULL,
     data JSONB NULL
 )
-TABLESPACE fieldsets;
+TABLESPACE pipeline;
