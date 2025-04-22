096a103a6d4ceefb69eda8fe600f214f8c351d19
willgladstone <will.gl@dst.one>
2025-04-22 17:09:41 -0400

    fixes for new phase logic

--- initdb/sql/tables/fieldsets.lookups.sql
+++ initdb/sql/tables/fieldsets.lookups.sql
@@ -1,13 +1,14 @@
 /**
  * Lookup Data Store
  * Partitioned on PostgreSQL side for performance.
- * Lookups associate a given fieldset id with another.
+ * Lookups associate a given fieldset id with a value.
+ * Values are indexed and relationships can be 1 to 1, 1 to many or many to 1
  */
 CREATE TABLE IF NOT EXISTS fieldsets.lookups (
     id              BIGINT NOT NULL,
     parent          BIGINT NOT NULL,
-    field_id        BIGINT NOT NULL,
-    type            FIELD_TYPE NOT NULL,
-    value           FIELD_VALUE NOT NULL
+    value           FIELD_VALUE NOT NULL,
+    created         TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated         TIMESTAMPTZ NOT NULL DEFAULT NOW()
 ) PARTITION BY LIST(parent)
 TABLESPACE lookups;
